ee9d39b2259060c0f19a57c59cee37a080a2f177
vibha-k <85023194+vibha-k@users.noreply.github.com>
2021-05-29 12:01:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("hello world")
+import matplotlib.pyplot as plot
+from scipy import stats
+
+x = []
+y = []
+
+print("LINEAR REGRESSION CALCULATOR")
+numData = int(input("How many coordinates are being entered: "))
+
+for i in range(numData): 
+  print("Coordinate %d" % (i+1))
+  xVal = int(input("x-coordinate: "))
+  yVal = int(input("y-coordinate: "))
+
+  x.append(xVal)
+  y.append(yVal)
+
+print("Your linear regression line has been created")
+
+slope, intercept, r, p, error = stats.linregress(x, y)
+
+print()
+print("LINEAR REGRESSION INFORMATION: ")
+print("Slope: %f" % slope)
+print("Y-Intercept: %f" % intercept)
+print("R-value: %f" % r); 
+print("Standard Error: %f" % error)
+
+def calculateYCoor(x): 
+  return slope * x + intercept
+
+graph = list(map(calculateYCoor, x))
+
+plot.scatter(x, y)
+plot.plot(x, graph)
+plot.show()
